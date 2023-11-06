93c83eebb465f7a0ac16e7fc3e3a5a59cc3f0f4b
Leeo17 <leeo.oneda@gmail.com>
2023-11-06 18:31:41 -0300

    feat: functional navbar

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateUserComponent } from './create-user/create-user.component';
+import { ExportComponent } from './export/export.component';
+import { NewAnalysisComponent } from './new-analysis/new-analysis.component';
+import { ResultsComponent } from './results/results.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'create-user', pathMatch: 'prefix' },
+  { path: '', redirectTo: 'new-analysis', pathMatch: 'prefix' },
+  { path: 'new-analysis', component: NewAnalysisComponent },
+  { path: 'results', component: ResultsComponent },
+  { path: 'export', component: ExportComponent },
   { path: 'create-user', component: CreateUserComponent },
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,9 +8,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { NewAnalysisComponent } from './new-analysis/new-analysis.component';
+import { ResultsComponent } from './results/results.component';
+import { ExportComponent } from './export/export.component';
 
 @NgModule({
-  declarations: [CreateUserComponent],
+  declarations: [CreateUserComponent, NewAnalysisComponent, ResultsComponent, ExportComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
