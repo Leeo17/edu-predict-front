5447b7cd507ce6a4b5ddb892304d7e97c676db43
Leeo17 <leeo.oneda@gmail.com>
2023-11-21 20:50:33 -0300

    feat: add new fields to analysis page

---
+++ src/app/home/consts/select-options.const.ts
@@ -0,0 +1,69 @@
+export const COR_RACA_OPTIONS = ['Branca', 'Parda', 'Amarela', 'Preta', 'Indígena', 'Desconhecido'];
+
+export const COTA_SISU_OPTIONS = [
+  'Não informado',
+  'Concorrência Geral',
+  'Independente da renda + ensino médio',
+  'Renda familiar =< 1,5 + ensino médio',
+  'Pretos, pardos ou indígenas + ens.médio',
+  'Pretos, pardos ou indígenas =< 1,5 + ens',
+  'Portador de Deficiência',
+  'Defic. renda =< 1,5 + ensino médio',
+  'Pretos,pardos ou indígenas + ens.medio p',
+  'Defic. independ. da renda + ensino médio',
+  'Defic,pretos,pardos ou indígenas =< 1,5',
+];
+
+export const ATIVIDADE_REMUNERADA_OPTIONS = ['Antes dos 16', 'Depois dos 16', 'Nunca trabalhei'];
+
+export const ESTUDOS_OPTIONS = [
+  'Integralmente em escola pública',
+  'Maior parte em escola pública',
+  'Integralmente em escola particular',
+  'Maior parte em escola particular',
+  'Em escolas comunitárias/CNEC ou outro',
+];
+
+export const LINGUA_ESTRANGEIRA_OPTIONS = [
+  'Não leio em nenhuma Língua Estrangeira',
+  'Leio apenas em Inglês',
+  'Leio apenas em Espanhol',
+  'Leio apenas em Frânces',
+  'Leio apenas uma outra Língua Estrangeira',
+  'Leio duas ou mais línguas estrangeiras',
+];
+
+export const FATORES_OPTIONS = [
+  'Conciliar estudos com vida pessoal e familiar',
+  'Persistência e hábitos de estudo',
+  'Habilidades específicas exigidas pelo curso',
+  'Informações da profissão',
+  'Relacionamentos',
+  'Não se enquadra nas alternativas anteriores',
+];
+
+export const TRABALHO_OPTIONS = [
+  'Sim, desde o primeiro ano, em tempo parcial',
+  'Sim, desde o primeiro ano, em tempo integral',
+  'Sim, mas apenas nos últimos anos',
+  'Não',
+  'Não sei',
+];
+
+export const TURNO_ENSINO_MEDIO_OPTIONS = [
+  'Todo diurno',
+  'Todo noturno',
+  'Maior parte diurno',
+  'Maior parte noturno',
+  'Outro',
+];
+
+export const TIPO_ENSINO_MEDIO_OPTIONS = [
+  'Ensino médio regular',
+  'Ensino médio com terceirão ou cursinho',
+  'Curso profissionalizante',
+  'Curso de magistério',
+  'Outro',
+];
+
+export const SIM_NAO_OPTIONS = ['Sim', 'Não', 'Sem Resposta'];
--- src/app/home/new-analysis/new-analysis.component.ts
+++ src/app/home/new-analysis/new-analysis.component.ts
@@ -3,6 +3,18 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable, startWith, switchMap } from 'rxjs';
 import { Course } from '../../shared/interfaces';
 import { ApiService } from '../../shared/services/api.service';
+import {
+  ATIVIDADE_REMUNERADA_OPTIONS,
+  COR_RACA_OPTIONS,
+  COTA_SISU_OPTIONS,
+  ESTUDOS_OPTIONS,
+  FATORES_OPTIONS,
+  LINGUA_ESTRANGEIRA_OPTIONS,
+  SIM_NAO_OPTIONS,
+  TIPO_ENSINO_MEDIO_OPTIONS,
+  TRABALHO_OPTIONS,
+  TURNO_ENSINO_MEDIO_OPTIONS,
+} from '../consts';
 
 @Component({
   selector: 'app-new-analysis',
@@ -13,6 +25,16 @@ export class NewAnalysisComponent implements OnInit {
   dadosAcademicosForm!: FormGroup;
   isLoading = false;
   cursos: Observable<Course[]> | undefined;
+  corRacaOptions = COR_RACA_OPTIONS;
+  cotaSisuOptions = COTA_SISU_OPTIONS;
+  atividadeRemuneradaOptions = ATIVIDADE_REMUNERADA_OPTIONS;
+  estudosOptions = ESTUDOS_OPTIONS;
+  linguaEstrangeiraOptions = LINGUA_ESTRANGEIRA_OPTIONS;
+  fatoresOptions = FATORES_OPTIONS;
+  trabalhoOptions = TRABALHO_OPTIONS;
+  turnoEnsinoMedioOptions = TURNO_ENSINO_MEDIO_OPTIONS;
+  tipoEnsinoMedioOptions = TIPO_ENSINO_MEDIO_OPTIONS;
+  simNaoOptions = SIM_NAO_OPTIONS;
 
   constructor(private formBuilder: FormBuilder, private apiService: ApiService) {}
 
